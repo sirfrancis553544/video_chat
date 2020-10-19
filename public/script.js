75a2dde4f56bd7d9cf295b64f674a0651b8f25dd
SirFrancis <55309898+sirfrancis553544@users.noreply.github.com>
2020-10-19 19:58:28 +0800

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,18 @@
+const socket = io("/");
+const videoGrid = document.getElementById("video-grid");
+
+const myPeer = new Peer(undefined, {
+  host: "/",
+  port: "3001",
+});
+
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+myPeer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+socket.on("user-connected", (userId) => {
+  console.log("User connected: " + userId);
+});
